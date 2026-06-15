cd0375cbf853db1205f39d5381dd1ab8165340eb
Jeanca57707 <jean_duarte@ucne.edu.do>
2026-06-15 14:04:17 -0400

    Metodos agregarActividad y mostrarActividades

--- ejercicioCinco/Main.java
+++ ejercicioCinco/Main.java
@@ -1,23 +1,53 @@
 import java.util.*;
+import java.io.*;
 
 public class Main{
 
     //Manejo de archivos 
     public static void main(String[] args){
 
-
-
-
-
+        menu();
     }
 
-    public static void agregarActividad(){
+    public static void agregarActividad(Scanner sc){
+
+        System.out.print("Ingrese la actividad: ");
+        String actividad = sc.nextLine(); 
+
+        try(FileWriter agregar = new FileWriter("actividades.txt", true)){
 
 
+            agregar.write(actividad + System.lineSeparator());
+            System.out.println("\nActividad agregada.");
+        
+        }catch(IOException e){
+
+            System.out.println("Error al guardar el archivo.");
+        }
     }
 
     public static void mostrarActividad(){
 
+
+        try(BufferedReader lectura = new BufferedReader(new FileReader("actividades.txt"))){
+
+            String linea;
+
+            System.out.println("\nACTIVIDADES REGISTRADAS\n");
+
+            while((linea = lectura.readLine()) != null){
+
+                System.out.println(linea);
+            }
+
+        }catch(FileNotFoundException e){
+
+            System.out.println("No hay atividades registradas aún.");
+        }
+        catch(IOException e){
+
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
 
     }
     public  static void menu(){
@@ -26,14 +56,13 @@ public class Main{
 
         int opc = 0;
 
-
         do{
 
             try{
 
                 System.out.println("\n1. Agrgar actividad.");
-                System.out.println("2. Mostrar actividades. ");
-                System.out.println("3.Salir.");
+                System.out.println("2. Mostrar actividades.");
+                System.out.println("3. Salir.");
                 
                 System.out.print("\nELija una opción: ");
                 opc = sc.nextInt();
@@ -41,34 +70,37 @@ public class Main{
 
                 switch(opc){
 
-
-
                     case 1:
 
+                        agregarActividad(sc);
                         break;
 
                     case 2:
 
+                        mostrarActividad();
                         break;
 
                     case 3:
 
+                        System.out.println("Saliendo del programa...");
                         break;
 
                     default:
 
-                        System.out.println("\Opcion invalida.");
+                        System.out.println("\nOpcion invalida.");
                         break;
                 }
 
             }catch(InputMismatchException e){
 
-                System.out.println(e.getMessage());
+                System.out.println("Error: Debe ingresar un opción válida.");
                 sc.nextLine();
             }
-
 
         }while(opc != 3);
         sc.close();
     }
 }
+
+   
+
